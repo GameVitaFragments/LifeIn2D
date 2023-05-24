6d254b58e3a269ac0752188e81f4caa66bc8d94a
Bharath33-Krishnan <bharathkrishnan33@gmail.com>
2023-05-24 23:52:15 +0530

    Added renderBuffer.js for shader rendering support

---
+++ classes/RenderBuffer.js
@@ -0,0 +1,35 @@
+//Define renderBuffer and renderShader in setup and preload respectively
+
+let renderBuffer;
+let renderShader;
+
+function drawScreen(shaderState) {
+
+  
+  renderShader.setUniform('texture',renderBuffer);
+  renderShader.setUniform('noise', getNoiseValue());
+  renderShader.setUniform('time', frameCount * 0.01);
+  renderShader.setUniform('active', shaderState);
+  push();
+  clear();
+  shader(renderShader);
+  rect(-width / 2, -height / 2, width, height);
+  resetShader();
+  pop();
+  fuzzyTex.clear();
+}
+
+
+
+function getNoiseValue() {
+  let v = sin(millis() / 10000);
+  const cutOff = 0.1;
+
+  if (v < cutOff) {
+    return 0;
+  }
+
+  v = pow((v - cutOff) * 1 / (1 - cutOff), 2);
+
+  return v;
+}
